4a21bfe051a186de2c41004ff45f09a80853373e
Efa-Iwa Eleng <gistend@gmail.com>
2023-01-05 04:22:41 +0000

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,32 @@
+var student1 = {
+    firstName: 'efa-iwa',
+    lastName: 'eleng',
+    age: 33,
+    location: 'lagos'
+};
+var student2 = {
+    firstName: 'alpha',
+    lastName: 'sage',
+    age: 28,
+    location: 'abuja'
+};
+var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
+table.style.background = "lightgray";
+table.appendChild(tbody);
+studentsList.forEach(function (student) {
+    var row = document.createElement('tr');
+    var nameCell = document.createElement('td');
+    var locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    nameCell.style.border = "2px solid white";
+    locationCell.style.border = "2px solid white";
+    nameCell.style.padding = "4px";
+    locationCell.style.padding = "4px";
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
+document.body.appendChild(table);
